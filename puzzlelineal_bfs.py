690d85a7092a4ba6c4dceef06c7d28dc6f6e0040
aldacova <aldacovaa@gmail.com>
2019-03-31 02:40:15 -0700

    v1.1

--- puzzlelineal_bfs.py
+++ puzzlelineal_bfs.py
@@ -9,7 +9,6 @@ def buscar_solucion_bfs(estado_inicial, solucion):
     nodos_frontera = []
     nodoInicial = Nodo(estado_inicial)
     nodos_frontera.append(nodoInicial)
-    nodos_frontera.append(nodoInicial)
     while (not solucionado) and len(nodos_frontera) != 0:
         nodo = nodos_frontera.pop(0)
         # Extraer nodo y anadirlo a visitados
@@ -25,20 +24,17 @@ def buscar_solucion_bfs(estado_inicial, solucion):
             # operador izquierdo
             hijo = [dato_nodo[1], dato_nodo[0], dato_nodo[2], dato_nodo[3]]
             hijo_izquierdo = Nodo(hijo)
-            if not hijo_izquierdo.en_lista(nodos_visitados) \
-            and not hijo_izquierdo.en_lista(nodos_frontera):
+            if not hijo_izquierdo.en_lista(nodos_visitados) and not hijo_izquierdo.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_izquierdo)
             # operador central
             hijo = [dato_nodo[0], dato_nodo[2], dato_nodo[1], dato_nodo[3]]
             hijo_central = Nodo(hijo)
-            if not hijo_central.en_lista(nodos_visitados) \
-            and not hijo_central.en_lista(nodos_frontera):
+            if not hijo_central.en_lista(nodos_visitados) and not hijo_central.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_central)
             # operador derecho
             hijo = [dato_nodo[0], dato_nodo[1], dato_nodo[3], dato_nodo[2]]
             hijo_derecho = Nodo(hijo)
-            if not hijo_derecho.en_lista(nodos_visitados) \
-            and not hijo_derecho.en_lista(nodos_frontera):
+            if not hijo_derecho.en_lista(nodos_visitados) and not hijo_derecho.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_derecho)
 
             nodo.set_hijos([hijo_izquierdo, hijo_central, hijo_derecho])
